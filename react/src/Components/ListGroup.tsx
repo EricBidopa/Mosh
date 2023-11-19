b4cd33b5280dd1a10eb87aa0e1255bb0bf1e31c2
unknown <you@example.com>
2023-11-19 17:41:38 +0000

    Bidopa

---
+++ react/src/Components/ListGroup.tsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+    const items = [
+        'New York',
+        'Tokyo',
+        'San Francisco',
+        'London',
+        'Paris'
+    ]
+  return (
+    <>
+        <h1>List</h1>
+    <ul className="list-group">
+    {items.map(item => <li>{item}</li>)}
+
+    </ul>
+
+    </>
+  );
+}
+
+export default ListGroup;
